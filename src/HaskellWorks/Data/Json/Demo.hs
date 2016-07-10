f843cbbcb7a561e57f20a6933292ee99fd207f39
John Ky <newhoggy@gmail.com>
2016-07-10 22:56:52 +1000

    How to load large json file

--- src/HaskellWorks/Data/Json/Demo.hs
+++ src/HaskellWorks/Data/Json/Demo.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module HaskellWorks.Data.Json.Demo where
 
-moo :: Int
-moo = 1
+import qualified Data.ByteString                                  as BS
+import qualified Data.Vector.Storable                             as DVS
+import           Data.Word
+import           GHC.Conc
+import           HaskellWorks.Data.Bits.BitShown
+import           HaskellWorks.Data.FromByteString
+import           HaskellWorks.Data.Json.Succinct.Cursor
+import           HaskellWorks.Data.Succinct.BalancedParens.Simple
+import           HaskellWorks.Diagnostics.Time
+import           System.Mem
+
+readJson :: String -> IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
+readJson path = do
+  bs <- BS.readFile path
+  print "Read file"
+  !cursor <- measure (fromByteString bs :: JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
+  print "Created cursor"
+  return cursor
+
+loadJson78 :: IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
+loadJson78 = readJson "data/78mb.json"
