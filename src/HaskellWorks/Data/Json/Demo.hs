d84e0e3b764a4a69bae80699b736b1dc8a15416d
John Ky <newhoggy@gmail.com>
2016-07-16 11:18:31 +1000

    Stuff

--- src/HaskellWorks/Data/Json/Demo.hs
+++ src/HaskellWorks/Data/Json/Demo.hs
@@ -13,6 +13,29 @@ import           HaskellWorks.Data.Json.Succinct.Cursor
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
 import           HaskellWorks.Diagnostics.Time
 import           System.Mem
+import           HaskellWorks.Data.TreeCursor
+
+import           Control.Monad
+import qualified Data.ByteString                                            as BS
+import           Data.String
+import qualified Data.Vector.Storable                                       as DVS
+import           Data.Word
+import           HaskellWorks.Data.Bits.BitShown
+import           HaskellWorks.Data.Bits.BitWise
+import           HaskellWorks.Data.Decode
+import           HaskellWorks.Data.FromForeignRegion
+import           HaskellWorks.Data.Json.Succinct.Cursor                     as C
+import           HaskellWorks.Data.Json.Succinct.Index
+import           HaskellWorks.Data.Json.Value
+import           HaskellWorks.Data.Succinct.BalancedParens.Internal
+import           HaskellWorks.Data.Succinct.BalancedParens.Simple
+import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank0
+import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank1
+import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1
+import           HaskellWorks.Data.Succinct.RankSelect.Binary.Poppy512
+import qualified HaskellWorks.Data.TreeCursor                               as TC
+
+newtype QueryJsonCursor = QueryJsonCursor [JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64))]
 
 readJson :: String -> IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
 readJson path = do
@@ -22,5 +45,21 @@ readJson path = do
   print "Created cursor"
   return cursor
 
-loadJson78 :: IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
-loadJson78 = readJson "data/78mb.json"
+-- (/@) :: [JsonValue] -> String -> [JsonValue]
+-- (/@) js fieldName = js >>= \j -> do
+--   case j of
+--     JsonObject ks -> ks
+--     _ ->
+--   return j
+
+loadJson :: String -> IO (Either DecodeError [JsonValue])
+loadJson filename = do
+  !cursor <- readJson filename
+  let !jsonResult = (jsonIndexAt >=> jsonValueAt) cursor
+  return $ (:[]) `fmap` jsonResult
+
+loadJson78 :: IO (Either DecodeError [JsonValue])
+loadJson78 = do
+  !cursor <- readJson "data/78mbs.json"
+  let !jsonResult = (jsonIndexAt >=> jsonValueAt) cursor
+  return $ (:[]) `fmap` jsonResult
