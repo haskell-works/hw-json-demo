509fe0cb7f14d8264351b73a445cbe2b58979ce2
John Ky <newhoggy@gmail.com>
2016-07-19 22:58:43 +1000

    Implement difference list for performance

--- src/HaskellWorks/Data/Json/Demo.hs
+++ src/HaskellWorks/Data/Json/Demo.hs
@@ -1,11 +1,16 @@
-{-# LANGUAGE BangPatterns        #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 
 module HaskellWorks.Data.Json.Demo where
 
 import qualified Data.ByteString                                  as BS
+import           Data.List
 import qualified Data.Vector.Storable                             as DVS
 import           Data.Word
+import           GHC.Base
 import           GHC.Conc
 import           HaskellWorks.Data.Bits.BitShown
 import           HaskellWorks.Data.FromByteString
@@ -17,6 +22,7 @@ import           HaskellWorks.Data.TreeCursor
 
 import           Control.Monad
 import qualified Data.ByteString                                            as BS
+import qualified Data.DList                                                 as DL
 import           Data.String
 import qualified Data.Vector.Storable                                       as DVS
 import           Data.Word
@@ -24,8 +30,10 @@ import           HaskellWorks.Data.Bits.BitShown
 import           HaskellWorks.Data.Bits.BitWise
 import           HaskellWorks.Data.Decode
 import           HaskellWorks.Data.FromForeignRegion
+import           HaskellWorks.Data.Json.PartialValue
 import           HaskellWorks.Data.Json.Succinct.Cursor                     as C
 import           HaskellWorks.Data.Json.Succinct.Index
+import           HaskellWorks.Data.Json.Succinct.PartialIndex
 import           HaskellWorks.Data.Json.Value
 import           HaskellWorks.Data.Succinct.BalancedParens.Internal
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
@@ -45,21 +53,59 @@ readJson path = do
   print "Created cursor"
   return cursor
 
--- (/@) :: [JsonValue] -> String -> [JsonValue]
--- (/@) js fieldName = js >>= \j -> do
---   case j of
---     JsonObject ks -> ks
---     _ ->
---   return j
-
 loadJson :: String -> IO (Either DecodeError [JsonValue])
 loadJson filename = do
   !cursor <- readJson filename
   let !jsonResult = (jsonIndexAt >=> jsonValueAt) cursor
   return $ (:[]) `fmap` jsonResult
 
-loadJson78 :: IO (Either DecodeError [JsonValue])
-loadJson78 = do
-  !cursor <- readJson "data/78mbs.json"
-  let !jsonResult = (jsonIndexAt >=> jsonValueAt) cursor
-  return $ (:[]) `fmap` jsonResult
+loadJsonPartial :: String -> IO JsonPartialValue
+loadJsonPartial filename = do
+  !cursor <- readJson filename
+  let !jsonResult = jsonPartialJsonValueAt (jsonPartialIndexAt cursor)
+  return jsonResult
+
+expandArray :: JsonPartialValue -> QuerySet JsonPartialValue
+expandArray jpv = case jpv of
+  JsonPartialArray es -> QuerySet $ DL.fromList es
+  _                   -> QuerySet   DL.empty
+
+expandObject :: JsonPartialValue -> QuerySet JsonPartialValue
+expandObject jpv = case jpv of
+  JsonPartialObject fs  -> QuerySet $ DL.fromList (snd `map` fs)
+  _                     -> QuerySet   DL.empty
+
+jsonKeys :: JsonPartialValue -> [String]
+jsonKeys jpv = case jpv of
+  JsonPartialObject fs  -> fst `map` fs
+  _                     -> []
+
+hasKey :: String -> JsonPartialValue -> Bool
+hasKey fieldName jpv = fieldName `elem` jsonKeys jpv
+
+inArray :: QuerySet JsonPartialValue -> QuerySet JsonPartialValue
+inArray jpvs = jpvs >>= expandArray
+
+jsonSize :: JsonPartialValue -> QuerySet JsonPartialValue
+jsonSize jpv = case jpv of
+  JsonPartialArray  es  -> QuerySet (DL.singleton (JsonPartialNumber (fromIntegral (length es))))
+  JsonPartialObject es  -> QuerySet (DL.singleton (JsonPartialNumber (fromIntegral (length es))))
+  _                     -> QuerySet (DL.singleton (JsonPartialNumber 0))
+
+newtype QuerySet a = QuerySet (DL.DList a)
+
+deriving instance Functor     QuerySet
+deriving instance Applicative QuerySet
+deriving instance Monad       QuerySet
+deriving instance Alternative QuerySet
+deriving instance MonadPlus   QuerySet
+
+instance Show a => Show (Mini (DL.DList a)) where
+  showsPrec _ (Mini dxs) = case DL.toList dxs of
+    xs@(_:_:_:_:_:_:_:_:_:_:_:_:_)  -> (("[" ++ intercalate ", " (show `map` take 10 xs) ++ ", ..]") ++)
+    []                              -> shows "[]"
+    xs                              -> (("[" ++ intercalate ", " (show `map` xs) ++ "]") ++)
+
+instance Show (QuerySet JsonPartialValue) where
+  -- show (QuerySet jpvs) = show (Mini (Mini `map` jpvs))
+  showsPrec _ (QuerySet das) = shows (Mini (Mini `fmap` das))
