c0339f39b9ba3ee315ffb3c1986e5981f5247361
John Ky <newhoggy@gmail.com>
2016-07-20 08:06:03 +1000

    Fix warnings

--- src/HaskellWorks/Data/Json/Demo.hs
+++ src/HaskellWorks/Data/Json/Demo.hs
@@ -6,51 +6,32 @@
 
 module HaskellWorks.Data.Json.Demo where
 
+import           Control.Monad
 import qualified Data.ByteString                                  as BS
+import qualified Data.DList                                       as DL
 import           Data.List
 import qualified Data.Vector.Storable                             as DVS
 import           Data.Word
 import           GHC.Base
-import           GHC.Conc
 import           HaskellWorks.Data.Bits.BitShown
-import           HaskellWorks.Data.FromByteString
-import           HaskellWorks.Data.Json.Succinct.Cursor
-import           HaskellWorks.Data.Succinct.BalancedParens.Simple
-import           HaskellWorks.Diagnostics.Time
-import           System.Mem
-import           HaskellWorks.Data.TreeCursor
-
-import           Control.Monad
-import qualified Data.ByteString                                            as BS
-import qualified Data.DList                                                 as DL
-import           Data.String
-import qualified Data.Vector.Storable                                       as DVS
-import           Data.Word
-import           HaskellWorks.Data.Bits.BitShown
-import           HaskellWorks.Data.Bits.BitWise
 import           HaskellWorks.Data.Decode
-import           HaskellWorks.Data.FromForeignRegion
+import           HaskellWorks.Data.FromByteString
 import           HaskellWorks.Data.Json.PartialValue
-import           HaskellWorks.Data.Json.Succinct.Cursor                     as C
+import           HaskellWorks.Data.Json.Succinct.Cursor
 import           HaskellWorks.Data.Json.Succinct.Index
 import           HaskellWorks.Data.Json.Succinct.PartialIndex
 import           HaskellWorks.Data.Json.Value
-import           HaskellWorks.Data.Succinct.BalancedParens.Internal
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank0
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank1
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Poppy512
-import qualified HaskellWorks.Data.TreeCursor                               as TC
+import           HaskellWorks.Diagnostics.Time
 
 newtype QueryJsonCursor = QueryJsonCursor [JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64))]
 
 readJson :: String -> IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
 readJson path = do
   bs <- BS.readFile path
-  print "Read file"
+  putStrLn "Read file"
   !cursor <- measure (fromByteString bs :: JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
-  print "Created cursor"
+  putStrLn "Created cursor"
   return cursor
 
 loadJson :: String -> IO (Either DecodeError [JsonValue])
@@ -103,9 +84,8 @@ deriving instance MonadPlus   QuerySet
 instance Show a => Show (Mini (DL.DList a)) where
   showsPrec _ (Mini dxs) = case DL.toList dxs of
     xs@(_:_:_:_:_:_:_:_:_:_:_:_:_)  -> (("[" ++ intercalate ", " (show `map` take 10 xs) ++ ", ..]") ++)
-    []                              -> shows "[]"
+    []                              -> ("[]" ++)
     xs                              -> (("[" ++ intercalate ", " (show `map` xs) ++ "]") ++)
 
 instance Show (QuerySet JsonPartialValue) where
-  -- show (QuerySet jpvs) = show (Mini (Mini `map` jpvs))
   showsPrec _ (QuerySet das) = shows (Mini (Mini `fmap` das))
