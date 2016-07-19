6f70aa3e636a900cce721dfa6cbf2fe1b9910f17
John Ky <newhoggy@gmail.com>
2016-07-20 08:16:07 +1000

    New Show instance for Mini (String, JsonPartialArray)

--- src/HaskellWorks/Data/Json/Demo.hs
+++ src/HaskellWorks/Data/Json/Demo.hs
@@ -24,6 +24,46 @@ import           HaskellWorks.Data.Json.Value
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
 import           HaskellWorks.Diagnostics.Time
 
+newtype Mini a = Mini a
+
+class MiniShow a where
+  miniShows :: a -> String -> String
+  miniShow :: a -> String
+  miniShow a = miniShows a []
+
+instance MiniShow JsonPartialValue where
+  miniShows v = case v of
+    JsonPartialString s   -> shows s
+    JsonPartialNumber n   -> shows n
+    JsonPartialObject []  -> ("{}" ++)
+    JsonPartialObject _   -> ("{..}" ++)
+    JsonPartialArray []   -> ("[]" ++)
+    JsonPartialArray _    -> ("[..]" ++)
+    JsonPartialBool w     -> shows w
+    JsonPartialNull       -> ("null" ++)
+    JsonPartialError s    -> ("<error " ++). shows s . (">" ++)
+
+instance Show (Mini JsonPartialValue) where
+  showsPrec _ v = case v of
+    Mini (JsonPartialString s ) -> shows s
+    Mini (JsonPartialNumber n ) -> shows n
+    Mini (JsonPartialObject []) -> ("{}" ++)
+    Mini (JsonPartialObject _ ) -> ("{..}" ++)
+    Mini (JsonPartialArray [] ) -> ("[]" ++)
+    Mini (JsonPartialArray _  ) -> ("[..]" ++)
+    Mini (JsonPartialBool w   ) -> shows w
+    Mini  JsonPartialNull       -> ("null" ++)
+    Mini (JsonPartialError s  ) -> ("<error " ++) . shows s . (">" ++)
+
+instance Show (Mini (String, JsonPartialValue)) where
+  showsPrec _ (Mini (fieldName, jpv)) = shows fieldName . (": " ++) . shows (Mini jpv)
+
+instance Show a => Show (Mini [a]) where
+  show (Mini xs) = case length xs of
+    xsLen | xsLen == 0    -> "[]"
+    xsLen | xsLen <= 10   -> "[" ++ intercalate ", " (show `map` xs) ++ "]"
+    _                     -> "[" ++ intercalate ", " (show `map` take 10 xs) ++ ", ..]"
+
 newtype QueryJsonCursor = QueryJsonCursor [JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64))]
 
 readJson :: String -> IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
@@ -51,9 +91,9 @@ expandArray jpv = case jpv of
   JsonPartialArray es -> QuerySet $ DL.fromList es
   _                   -> QuerySet   DL.empty
 
-expandObject :: JsonPartialValue -> QuerySet JsonPartialValue
+expandObject :: JsonPartialValue -> QuerySet (String, JsonPartialValue)
 expandObject jpv = case jpv of
-  JsonPartialObject fs  -> QuerySet $ DL.fromList (snd `map` fs)
+  JsonPartialObject fs  -> QuerySet $ DL.fromList fs
   _                     -> QuerySet   DL.empty
 
 jsonKeys :: JsonPartialValue -> [String]
@@ -88,4 +128,7 @@ instance Show a => Show (Mini (DL.DList a)) where
     xs                              -> (("[" ++ intercalate ", " (show `map` xs) ++ "]") ++)
 
 instance Show (QuerySet JsonPartialValue) where
+  showsPrec _ (QuerySet das) = shows (Mini (Mini `fmap` das))
+
+instance Show (QuerySet (String, JsonPartialValue)) where
   showsPrec _ (QuerySet das) = shows (Mini (Mini `fmap` das))
